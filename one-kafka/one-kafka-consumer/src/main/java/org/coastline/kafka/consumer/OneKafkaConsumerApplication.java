33e53654f9cb97b36e060c1a1b27d654884c1936
zouhuajian <zouhuajianclever@gmail.com>
2020-05-04 10:42:33 +0800

    kafka

---
+++ one-kafka/one-kafka-consumer/src/main/java/org/coastline/kafka/consumer/OneKafkaConsumerApplication.java
@@ -0,0 +1,16 @@
+package org.coastline.kafka.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Jay
+ */
+@SpringBootApplication
+public class OneKafkaConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneKafkaConsumerApplication.class, args);
+	}
+
+}
---
+++ one-kafka/one-kafka-producer/src/test/java/org/coastline/kafka/consumer/OneKafkaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.coastline.kafka.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneKafkaProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
