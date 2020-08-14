baf46d35c132698302792ef3f94763829960d78a
Jay.H.Zou <Jay.H.Zou@newegg.com>
2020-08-14 17:56:53 +0800

    sentinel demo

---
+++ one-sentinel/src/main/java/org/coastline/one/sentinel/OneSentinelApplication.java
@@ -0,0 +1,15 @@
+package org.coastline.one.sentinel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Jay.H.Zou
+ * @date 8/14/2020
+ */
+@SpringBootApplication
+public class OneSentinelApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OneSentinelApplication.class, args);
+    }
+}
