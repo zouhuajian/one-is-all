d3a5059eb4dd1ff848ce00bcc20f5e6d741f8663
zouhuajian <zouhuajianclever@gmail.com>
2020-11-11 23:22:30 +0800

    CompareJson

--- one-common/src/main/java/org/coastline/common/compare/CompareJson.java
+++ one-common/src/main/java/org/coastline/common/compare/CompareJson.java
@@ -2,6 +2,9 @@ package org.coastline.common.compare;
 
 import com.alibaba.fastjson.JSONObject;
 
+import java.util.ArrayList;
+import java.util.Objects;
+
 /**
  * 深度对比 json
  * @author Jay.H.Zou
@@ -13,9 +16,23 @@ public class CompareJson {
     private static JSONObject replay = new JSONObject();
 
     public static void main(String[] args) {
+        origin.put("one", "one");
+        JSONObject two = new JSONObject();
+        two.put("two", new ArrayList<>());
+        origin.put("two", two);
+        origin.put("three", 2);
         origin.keySet().forEach(key -> {
             // TODO: 如何判断类型？是json还是string等
-            String string = replay.getString(key);
+            Object obj = origin.get(key);
+            if (obj instanceof String) {
+                System.out.println(obj);
+            }
+            if (obj instanceof JSONObject) {
+                System.out.println(obj);
+            }
+            if (obj instanceof Integer) {
+                System.out.println(obj);
+            }
 
         });
     }
