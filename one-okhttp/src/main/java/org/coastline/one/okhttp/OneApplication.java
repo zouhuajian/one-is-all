2c2c31a0ea241e7be4226fd73960e7a8e4eee549
zouhuajian <zouhuajianclever@gmail.com>
2022-05-30 00:09:18 +0800

    okhttp test

--- one-okhttp/src/main/java/org/coastline/one/okhttp/OneApplication.java
+++ one-okhttp/src/main/java/org/coastline/one/okhttp/OneApplication.java
@@ -34,43 +34,16 @@ public class OneApplication {
     private static final List<String> KAFKA_TOPICS = new ArrayList<String>() {{
         add("");
     }};
+    private static final List<KafkaConsumer<byte[], byte[]>> CONSUMERS = new ArrayList<>();
 
     // http
     public static final MediaType TEXT_PLAIN = MediaType.parse("text/plain;charset=utf-8");
     private static final String URL = "";
 
-
     private static final int CORE = Runtime.getRuntime().availableProcessors();
-    private static final List<KafkaConsumer<byte[], byte[]>> CONSUMERS = new ArrayList<>();
-    private static final ThreadPoolExecutor THREAD_POOL_EXECUTOR = new ThreadPoolExecutor(4, 4, 2, TimeUnit.MINUTES, new SynchronousQueue<>());
-
-    private static KafkaConsumer<byte[], byte[]> initKafkaClient() {
-        Properties properties = new Properties();
-        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_BROKERS);
-        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, KAFKA_GROUP_ID);
-        // serialize
-        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ByteArrayDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ByteArrayDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.FETCH_MAX_BYTES_CONFIG, String.valueOf(256 * 1024 * 1024));
-        properties.setProperty(ConsumerConfig.MAX_PARTITION_FETCH_BYTES_CONFIG, String.valueOf(256 * 1024 * 1024));
-        properties.setProperty(ConsumerConfig.REQUEST_TIMEOUT_MS_CONFIG, "60000");
-        properties.setProperty(ConsumerConfig.FETCH_MAX_WAIT_MS_CONFIG, "20000");
-        properties.setProperty(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "60000");
-        properties.setProperty(ConsumerConfig.HEARTBEAT_INTERVAL_MS_CONFIG, "20000");
-        KafkaConsumer<byte[], byte[]> consumer = new KafkaConsumer<>(properties);
-        consumer.subscribe(KAFKA_TOPICS, new ConsumerRebalanceListener() {
-            @Override
-            public void onPartitionsRevoked(Collection<TopicPartition> partitions) {
-                LOGGER.error("onPartitionsRevoked");
-            }
-
-            @Override
-            public void onPartitionsAssigned(Collection<TopicPartition> partitions) {
-                LOGGER.error("onPartitionsAssigned");
-            }
-        });
-        return consumer;
-    }
+    private static final ThreadPoolExecutor THREAD_POOL_EXECUTOR = new ThreadPoolExecutor(CORE, CORE,
+            2, TimeUnit.MINUTES,
+            new SynchronousQueue<>());
 
     public static void main(String[] args) throws InterruptedException {
         initShutdownHook();
@@ -127,6 +100,34 @@ public class OneApplication {
                     }
                 });
             }
+        }
+
+        private KafkaConsumer<byte[], byte[]> initKafkaClient() {
+            Properties properties = new Properties();
+            properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_BROKERS);
+            properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, KAFKA_GROUP_ID);
+            // serialize
+            properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ByteArrayDeserializer.class.getName());
+            properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ByteArrayDeserializer.class.getName());
+            properties.setProperty(ConsumerConfig.FETCH_MAX_BYTES_CONFIG, String.valueOf(256 * 1024 * 1024));
+            properties.setProperty(ConsumerConfig.MAX_PARTITION_FETCH_BYTES_CONFIG, String.valueOf(256 * 1024 * 1024));
+            properties.setProperty(ConsumerConfig.REQUEST_TIMEOUT_MS_CONFIG, "60000");
+            properties.setProperty(ConsumerConfig.FETCH_MAX_WAIT_MS_CONFIG, "20000");
+            properties.setProperty(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "60000");
+            properties.setProperty(ConsumerConfig.HEARTBEAT_INTERVAL_MS_CONFIG, "20000");
+            KafkaConsumer<byte[], byte[]> consumer = new KafkaConsumer<>(properties);
+            consumer.subscribe(KAFKA_TOPICS, new ConsumerRebalanceListener() {
+                @Override
+                public void onPartitionsRevoked(Collection<TopicPartition> partitions) {
+                    LOGGER.error("onPartitionsRevoked");
+                }
+
+                @Override
+                public void onPartitionsAssigned(Collection<TopicPartition> partitions) {
+                    LOGGER.error("onPartitionsAssigned");
+                }
+            });
+            return consumer;
         }
     }
 
