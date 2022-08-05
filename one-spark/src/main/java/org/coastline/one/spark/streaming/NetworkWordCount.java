b9e7c36b285dc7c23374782d60e039e1a3d5d893
zouhuajian <zouhuajian4450@100.me>
2022-08-05 19:13:02 +0800

    spark

---
+++ one-spark/src/main/java/org/coastline/one/spark/streaming/NetworkWordCount.java
@@ -0,0 +1,53 @@
+package org.coastline.one.spark.streaming;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaPairDStream;
+import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import scala.Tuple2;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+/**
+ * @author Jay.H.Zou
+ * @date 2022/8/1
+ */
+public class NetworkWordCount {
+
+    /**
+     * nc -lk 9999
+     *
+     * @param args
+     * @throws InterruptedException
+     */
+    public static void main(String[] args) throws InterruptedException {
+        // Create a local StreamingContext with two working thread and batch interval of 1 second
+        SparkConf conf = new SparkConf()
+                //.setMaster("local[2]")
+                .setMaster("spark://xxx:7077")
+                .setAppName("NetworkWordCount");
+        JavaStreamingContext jssc = new JavaStreamingContext(conf, Durations.seconds(10));
+        // Create a DStream that will connect to hostname:port, like localhost:9999
+        JavaReceiverInputDStream<String> lines = jssc.socketTextStream("localhost", 9999);
+        // Split each line into words
+        JavaDStream<String> words = lines.flatMap(new FlatMapFunction<String, String>() {
+            @Override
+            public Iterator<String> call(String s) throws Exception {
+                return Arrays.asList(s.split(" ")).iterator();
+            }
+        });
+
+        // Count each word in each batch
+        JavaPairDStream<String, Integer> pairs = words.mapToPair(s -> new Tuple2<>(s, 1));
+        JavaPairDStream<String, Integer> wordCounts = pairs.reduceByKey(Integer::sum);
+
+        // Print the first ten elements of each RDD generated in this DStream to the console
+        wordCounts.print();
+        jssc.start();              // Start the computation
+        jssc.awaitTermination();   // Wait for the computation to terminate
+    }
+}
