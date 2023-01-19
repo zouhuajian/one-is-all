82457be2211985d883291414c6f22cfcaa222ed1
zouhuajian <zouhuajian@qiyi.com>
2023-01-19 15:04:54 +0800

    [common] chinese to pinyin

--- one-common/src/test/java/spi/TestSPI.java
+++ one-common/src/test/java/spi/TestSPI.java
@@ -1,17 +1,11 @@
 package spi;
 
-import org.coastline.one.common.java.spi.SPIService;
-import sun.misc.Service;
-
-import java.util.Iterator;
-import java.util.ServiceLoader;
-
 /**
  * @author Jay.H.Zou
  * @date 2021/12/27
  */
 public class TestSPI {
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         Iterator<SPIService> providers = Service.providers(SPIService.class);
         while (providers.hasNext()) {
             SPIService ser = providers.next();
@@ -23,5 +17,5 @@ public class TestSPI {
         for (SPIService ser : load) {
             ser.execute();
         }
-    }
+    }*/
 }
